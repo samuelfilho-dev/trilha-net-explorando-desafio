ad6f9e55c6125fcc49910c467b67986db5f6b77e
samuel-filho-c9 <samuel@c9digital.com.br>
2025-06-27 18:17:54 -0300

    FEAT: Implementar os TODOS

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,19 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+
+            int capacidade = Suite.Capacidade;
+            int quantidadeHospedes = hospedes.Count;
+
+            if (capacidade >= quantidadeHospedes)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new ArgumentException("Capacidade insuficiente para o número de hóspedes.");
             }
         }
 
@@ -35,23 +38,29 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+
+            if (Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.9M;
             }
 
             return valor;
